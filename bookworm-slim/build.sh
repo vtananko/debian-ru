3e89b93408363642896be78d155c16d73d5543f8
Vladimir Tananko <v.tananko@gmail.com>
2024-12-30 04:44:19 +0300

    Small modify building

--- bookworm-slim/build.sh
+++ bookworm-slim/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Build image on local PC and push it on ghcr.io
+
+DEBIAN_MAJOR="12"
+
 GHCR_PAT_FILE=~/prj/docker/github.pat
 GHCR_USERNAME=vtananko
 REPO_NAME="debian-ru"
@@ -14,24 +18,16 @@ DT=`date +%Y%m%d`
 TAG_SUFFIX="slim"
 TAG_NAME="$TAG_PREFIX-$DT-$TAG_SUFFIX"
 
-#docker image inspect -f '{{ .Id }}' $TAG_NAME > /dev/null 2>&1
-#if (( $? == 0 )) ; then
-#	echo "Docker image $TAG_NAME already exists!"
-#	exit 1
-#fi
 
 docker build -t $TAG_NAME \
 	--build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
-	--build-arg BUILD_VERSION=$TAG_NAME \
-	--build-arg VCS_REF=$(git rev-parse --short HEAD) \
 	.
 (( $? == 0 )) || exit 2
 
 VERSION=`docker run -t --rm $TAG_NAME cat /etc/debian_version | tr -d '\r'`
-MAJOR_VERSION="$(cut -d'.' -f1 <<<"$VERSION")"
 
 docker tag $TAG_NAME $TAG_PREFIX-$TAG_SUFFIX
-docker tag $TAG_NAME $TAG_PREFIX-$MAJOR_VERSION-$TAG_SUFFIX
+docker tag $TAG_NAME $TAG_PREFIX-$DEBIAN_MAJOR-$TAG_SUFFIX
 docker tag $TAG_NAME $TAG_PREFIX-$VERSION-$TAG_SUFFIX
 
 if [ "$1" == "ghcr" ]; then
@@ -40,8 +36,6 @@ if [ "$1" == "ghcr" ]; then
 		echo "Not have GitHub Personal Access Token file. Exit."
 		exit 3
 	fi
-	#echo "GHCR cat PAT file..."
-	#GHCR_PAT=`cat $GHCR_PAT_FILE`
 	echo "Docker login to ghcr.io..."
 	cat $GHCR_PAT_FILE | docker login ghcr.io -u $GHCR_USERNAME --password-stdin
 	echo "Docker push to ghcr.io..."
